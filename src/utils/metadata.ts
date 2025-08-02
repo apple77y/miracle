d62427aa2f38cb4e3242cc6790ac622c10900d2d
hun-yong.song <hun-yong.song@linecorp.com>
2025-08-02 23:35:59 +0900

    Update icon files for each OS

--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -84,6 +84,8 @@ export const getMetadata = (locale: Locale = 'ko'): Metadata => {
     },
     other: {
       "naver-site-verification": "4b6679762238a11e4b68367323ceb6a21e184e8f",
+      "msapplication-TileImage": "/ms-icon-144x144.png",
+      "msapplication-TileColor": "#ffffff",
     },
     alternates: {
       canonical: "https://miracle-pi.vercel.app",
@@ -95,12 +97,26 @@ export const getMetadata = (locale: Locale = 'ko'): Metadata => {
     },
     icons: {
       icon: [
-        { url: "/icon.svg", type: "image/svg+xml" },
-        { url: "/favicon.svg", type: "image/svg+xml" }
+        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        { url: "/favicon-96x96.png", sizes: "96x96", type: "image/png" },
+        { url: "/android-icon-192x192.png", sizes: "192x192", type: "image/png" }
       ],
       apple: [
-        { url: "/apple-icon.png", sizes: "180x180", type: "image/png" }
+        { url: "/apple-icon-57x57.png", sizes: "57x57", type: "image/png" },
+        { url: "/apple-icon-60x60.png", sizes: "60x60", type: "image/png" },
+        { url: "/apple-icon-72x72.png", sizes: "72x72", type: "image/png" },
+        { url: "/apple-icon-76x76.png", sizes: "76x76", type: "image/png" },
+        { url: "/apple-icon-114x114.png", sizes: "114x114", type: "image/png" },
+        { url: "/apple-icon-120x120.png", sizes: "120x120", type: "image/png" },
+        { url: "/apple-icon-144x144.png", sizes: "144x144", type: "image/png" },
+        { url: "/apple-icon-152x152.png", sizes: "152x152", type: "image/png" },
+        { url: "/apple-icon-180x180.png", sizes: "180x180", type: "image/png" }
       ],
+      other: [
+        { rel: "icon", url: "/favicon.ico" },
+        { rel: "icon", url: "/icon.svg", type: "image/svg+xml" }
+      ]
     },
     manifest: "/manifest.json",
     appleWebApp: {

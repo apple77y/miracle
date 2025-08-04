5f2b6acce034c9371d9f656501dc12686ffe6f04
hun-yong.song <hun-yong.song@linecorp.com>
2025-08-04 13:59:41 +0900

    Remove notification service worker

--- src/components/PWALayout.tsx
+++ src/components/PWALayout.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { useIsPWA } from '../hooks/useIsPWA';
-import { usePWAInstall } from '../hooks/usePWAInstall';
 import BottomNavigation from './ui/BottomNavigation';
-import ToastNotification from './ui/ToastNotification';
 
 interface PWALayoutProps {
   children: React.ReactNode;
@@ -12,24 +10,12 @@ interface PWALayoutProps {
 export default function PWALayout({ children }: PWALayoutProps) {
   const isPWA = useIsPWA();
   
-  // PWA 설치 및 노티피케이션 기능 초기화
-  const { toastVisible, toastData, setToastVisible } = usePWAInstall();
-
   return (
     <>
       <div className={isPWA ? 'pb-16' : ''}>
         {children}
       </div>
       {isPWA && <BottomNavigation />}
-      
-      {/* iOS용 Toast 노티피케이션 */}
-      <ToastNotification
-        title={toastData.title}
-        message={toastData.message}
-        isVisible={toastVisible}
-        onClose={() => setToastVisible(false)}
-        duration={4000}
-      />
     </>
   );
 }
